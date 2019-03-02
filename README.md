da53b35abd719da62ab86d5d756d941e052513a9
Ray Redondo <rdrpenguin04@gmail.com>
2019-03-02 11:54:58 -0600

    Tutorial 4

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # FoTCDemoTutorial
-Link to playlist: [click here](https://www.youtube.com/playlist?list=PLQB8lA1gaTTp1gJ-DKZQGOlvMSsXBisD6 "How to Make a Game - YouTube")
-
-Setup: watch [this video](https://www.youtube.com/watch?v=cHM5jb5335o "How to Make a Game: Episode 1: Setup - YouTube") to see setup for Eclipse. For other IDEs, add them in the comments of a video (or issues of this repo) and we will make instructions.
+Coming to a YouTube near you!
--- src/com/lightning/fotc/Main.java
+++ src/com/lightning/fotc/Main.java
@@ -1,6 +1,7 @@
 package com.lightning.fotc;
 
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import com.lightning.jpipeworks.Engine;
 import com.lightning.jpipeworks.Game;
@@ -13,10 +14,12 @@ public class Main {
     public static class FoTCDemoGame extends Game {
         private BGMPlayer music;
         private int fc;
+        private FoTCDemoBG bg;
         
         public void loadState(Engine engine, GameState state) {
             // Ignore state for now
             Sprite mainPlayer;
+            engine.things.add(bg = new FoTCDemoBG(engine));
             engine.things.add(mainPlayer = new Sprite(
                     "mainPlayer/r%04d.png",
                     new SpriteAI() {
@@ -29,10 +32,9 @@ public class Main {
                                 mainPlayer.frame++;
                                 if(mainPlayer.frame > 180) mainPlayer.frame = 180;
                             }
-                            float xVelocity = (float) Math.sin((float) mainPlayer.frame / 180 * Math.PI) * 2;
-                            float yVelocity = (float) Math.cos((float) mainPlayer.frame / 180 * Math.PI) * -2;
-                            System.out.println("Velocity: (x = " + xVelocity + ", y = " + yVelocity + ")");
-                            mainPlayer.x += xVelocity;
+                            float xVelocity = (float) Math.sin((float) mainPlayer.frame / 180 * Math.PI) * 20;
+                            float yVelocity = (float) Math.cos((float) mainPlayer.frame / 180 * Math.PI) * -20;
+                            bg.x += xVelocity;
                             mainPlayer.y += yVelocity;
 //                            if(mainPlayer.collision)
 //                                System.out.println("HIT");
@@ -46,6 +48,50 @@ public class Main {
             engine.things.add(music = new BGMPlayer("music/lvl1.wav", engine));
             music.startMusic = true;
         }
+    }
+    
+    public static class FoTCDemoBG extends Thing {
+    	public float x;
+    	private float[] starX = new float[MAX_STARS];
+    	private float[] starY = new float[MAX_STARS];
+    	private float[] starZ = new float[MAX_STARS];
+    	private Engine engine;
+    	
+    	public static final int MAX_STARS = 2000;
+    	
+    	public FoTCDemoBG(Engine engine) {
+    		this.engine = engine;
+    		resources = new ArrayList<>();
+    		for(int i = 0; i < MAX_STARS; i++) {
+    			starX[i] = (float) Math.random()*40-20;
+    			starY[i] = (float) Math.random()*40*9/16f-20*9/16f;
+    			starZ[i] = (float) Math.random()*18+2;
+    		}
+    	}
+    	
+    	public void update() {
+    		for(int i = 0; i < starX.length; i++) {
+    			starX[i] -= x/1024;
+    		}
+    		x = 0;
+    	}
+    	
+    	public void render() {
+    		for(int i = 0; i < starX.length; i++) {
+    			int x = (int) ((starX[i]/starZ[i]+1)*512);
+    			int y = (int) ((starY[i]/starZ[i]+1)*512);
+    			if(starX[i] < -20) {
+        			starX[i] = 20;
+        			starY[i] = (float) Math.random()*40*9/16f-20*9/16f;
+        			starZ[i] = (float) Math.random()*18+2;
+    			}
+
+    			engine.plotPixel(x, y, 255, 255, 255);
+    			engine.plotPixel(x, y+1, 255, 255, 255);
+    			engine.plotPixel(x+1, y, 255, 255, 255);
+    			engine.plotPixel(x+1, y+1, 255, 255, 255);
+    		}
+    	}
     }
     
     public static class FoTCBullet extends Thing {
